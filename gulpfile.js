e3b8a7aa8dc9bedc9680de36905c4741023196eb
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-07-04 19:29:37 +0100

    built site
    final content ready for testing

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('serve', ['build-jekyll'], function() {
     server: {
       baseDir: '_site/',
       routes: {
-        '/_templates/destination-single': '_site/'
+        '/it/thailandia': '_site/'
       }
     }
   });
